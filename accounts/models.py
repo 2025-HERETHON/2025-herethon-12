bf68610e5503bc115beabde4b0ef7c825abb6537
yeessonng <dorlek856@gmail.com>
2025-07-04 14:25:44 +0900

    feat: 아이디 중복 확인 처리 보완, 회원 가입 기능 구현

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Member(AbstractUser):
     username = models.CharField(max_length=12, unique=True)
     nickname = models.CharField(max_length=10)
-    region_city = models.CharField(max_length=15)
+    region_city = models.CharField(max_length=15 )
     region_district = models.CharField(max_length=15)
     #is_verified = models.BooleanField(default=False)
     star = models.FloatField(default=0)
