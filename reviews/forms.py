8f4632ede26eb0994942d56b0e55ec1738c20923
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-07-05 15:00:36 +0900

    fix: Review 마이그레이션 오류 수정

--- reviews/forms.py
+++ reviews/forms.py
@@ -4,4 +4,4 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['rating', 'content', 'image_url']  # image_url은 이미지 업로드라면 FileField로 바꾸는 것도 가능
+        fields = ['rating', 'content', 'image']  # image_url은 이미지 업로드라면 FileField로 바꾸는 것도 가능
