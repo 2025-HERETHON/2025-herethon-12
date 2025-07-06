f5b929056639c67083a689ea2751617f1f87fffd
yeessonng <dorlek856@gmail.com>
2025-07-07 01:09:29 +0900

    feat: user, post모델에 컬럼 추가

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,7 @@ class Member(AbstractUser):
     nickname = models.CharField(max_length=10)
     region_city = models.CharField(max_length=15 )
     region_district = models.CharField(max_length=15)
+    region_dong = models.CharField(max_length=15)
     #is_verified = models.BooleanField(default=False)
     star = models.FloatField(default=0)
     image = models.ImageField(upload_to='profile_images/', null=True, blank=True)    #objects = MemberManager()
