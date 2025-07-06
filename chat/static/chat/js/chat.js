d650919500e3c6d064b16420138d76d858208277
zer0p01nt <1027.bmy@gmail.com>
2025-07-06 23:15:19 +0900

    feat: 쪽지방 UI 구현

---
+++ chat/static/chat/js/chat.js
@@ -0,0 +1,27 @@
+const darken = document.querySelector(".darken");
+const completeModal = document.querySelector(".complete-modal");
+const cancel = document.getElementById("cancel");
+const completeBtn = document.getElementById("complete-btn");
+const imgInput = document.getElementById("img-input");
+
+// 모달창 닫는 함수
+function closeAll() {
+  darken.classList.add("hidden");
+  completeModal.classList.add("hidden");
+}
+
+// 거래완료 버튼 누르면 화면 어두워지고 모달창 뜸
+completeBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  darken.classList.remove("hidden");
+  completeModal.classList.remove("hidden");
+});
+
+// 화면 모달창 닫히게 하기 위해, e 상위요소 전파 막음
+completeModal.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+// 취소 버튼 누르거나, 화면 다른 부분 누르면 모달창 닫힘
+document.addEventListener("click", closeAll);
+cancel.addEventListener("click", closeAll);
