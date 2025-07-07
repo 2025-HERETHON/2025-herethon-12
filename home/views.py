095f6163c279e5c0cf651f3bd24493e48b81d7bb
yeessonng <dorlek856@gmail.com>
2025-07-07 10:28:33 +0900

    feat: home 백엔드 코드와 연결, time filter 생성

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from posts.models import Item
 
 # Create your views here.
+@login_required
 def home_view(request):
-    items = Item.objects.all().order_by("-created_at")[:10]
+    items = Item.objects.all().order_by("-created_at")[:10] #최신순 10개 > 추후 수정 필요
     return render(request, "home/home_main.html", {"items": items})
