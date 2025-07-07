1b2d0bf768b0df8a97416016004329adcbf31bb8
soyu <soyou5876@naver.com>
2025-07-07 12:56:38 +0900

    feat: 프로필 별점기능/디테일 수정

---
+++ home/static/home/js/profile.js
@@ -0,0 +1,16 @@
+  const ratingBoxes = document.querySelectorAll(".rating-box");
+
+  ratingBoxes.forEach((box) => {
+    const scoreBox = box.querySelector(".rating-score");
+    const stars = box.querySelectorAll(".star-icon");
+    const score = parseFloat(scoreBox.textContent.trim());
+    const fill = Math.round(score * 2);
+
+    for (let i = 0; i < stars.length; i++) {
+      if (i < fill) {
+        stars[i].classList.add("star-icon-filled");
+      } else {
+        stars[i].classList.remove("star-icon-filled");
+      }
+    }
+  });
