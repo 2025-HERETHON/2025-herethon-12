eb20aa9453fd3a4ad369ffdbeedd7f917db53df6
zer0p01nt <1027.bmy@gmail.com>
2025-07-10 21:59:55 +0900

    fix: 나의 후기 페이지 드롭다운 추가

--- reviews/static/reviews/js/myreview.js
+++ reviews/static/reviews/js/myreview.js
@@ -134,3 +134,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+/* 받은 후기, 작성 후기 드롭다운 */
+const selected = document.querySelector(".selected");
+const options = document.querySelector(".options");
+
+// 드롭다운 누르면 열리고 다시 누르면 닫힘
+selected.addEventListener("click", (e) => {
+  e.stopPropagation();
+  options.classList.toggle("hidden");
+});
+
+// 드롭다운 열린 상태에서 드롭다운 제외 영역 누르면 닫힘
+document.addEventListener("click", () => {
+  if (!options.classList.contains("hidden")) {
+    options.classList.add("hidden");
+  }
+});
